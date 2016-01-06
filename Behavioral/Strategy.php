6dd10fb91c22d87752b990341688e7513c64ec56
Edin Omeragic <edin.omeragic@gmail.com>
2016-01-06 21:10:30 +0100

    PHP Design Pattern Samples

--- Behavioral/Strategy.php
+++ Behavioral/Strategy.php
@@ -1,2 +1,56 @@
 <?php
 
+interface IStrategy
+{
+    function Move(Context $context) : int;
+}
+
+class Strategy1 implements IStrategy
+{
+    function Move(Context $context) : int
+    {
+        $context->counter += 1;
+        return $context->counter;
+    }
+}
+
+class Strategy2 implements IStrategy
+{
+    function Move(Context $context) : int
+    {
+        $context->counter -= 1;
+        return $context->counter;
+    }
+}
+
+class Context
+{
+    private $strategy;
+    public $counter = 0;
+    
+    public function setStrategy(IStrategy $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+    
+    public function Algorithm() : int
+    {
+        return $this->strategy->Move($this); 
+    }
+}
+
+
+$context = new Context;
+
+$strategy1 = new Strategy1;
+$strategy2 = new Strategy2;
+
+$context->setStrategy($strategy1);
+
+for ($i = 0; $i < 30; $i ++)
+{
+    if ($i == 15) {
+        $context->setStrategy($strategy2);
+    }    
+    echo "Counter = ", $context->Algorithm(), "\n";
+}
